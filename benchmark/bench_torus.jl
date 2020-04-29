e47822474a5f0c38b398cc78f286319f035f8a92
mtsch <matijacufar@gmail.com>
2020-04-29 09:44:48 +0200

    we don't need to benchmark everything with different modulus

--- benchmark/bench_torus.jl
+++ benchmark/bench_torus.jl
@@ -4,26 +4,24 @@ using BenchmarkTools
 include(joinpath(@__DIR__, "../test/data.jl"))
 suite = BenchmarkGroup()
 
-for (npoints, dim_max, threshold) in ((1024, 1, 1),
-                                      (1024, 1, nothing),
-                                      (256, 2, 1),
-                                      (256, 2, nothing),
-                                      (64, 4, nothing),
-                                      (128, 4, 1))
-    for modulus in (2, 7)
-        name = "n=$npoints, dim_max=$dim_max, modulus=$modulus, threshold=$threshold"
-        if isnothing(threshold)
-            bench = @benchmarkable ripserer($(torus(npoints)),
-                                            dim_max=$dim_max,
-                                            modulus=$modulus)
-        else
-            bench = @benchmarkable ripserer($(torus(npoints)),
-                                            dim_max=$dim_max,
-                                            modulus=$modulus,
-                                            threshold=$threshold)
-        end
-        suite[name] = bench
+for (npoints, dim_max, threshold, modulus) in ((1024, 1, 1, 2),
+                                               (1024, 1, nothing, 7),
+                                               (256, 2, 1, 7),
+                                               (256, 2, nothing, 2),
+                                               (64, 4, nothing, 2),
+                                               (81, 4, 1, 3))
+    name = "n=$npoints, dim_max=$dim_max, modulus=$modulus, threshold=$threshold"
+    if isnothing(threshold)
+        bench = @benchmarkable ripserer($(torus(npoints)),
+                                        dim_max=$dim_max,
+                                        modulus=$modulus)
+    else
+        bench = @benchmarkable ripserer($(torus(npoints)),
+                                        dim_max=$dim_max,
+                                        modulus=$modulus,
+                                        threshold=$threshold)
     end
+    suite[name] = bench
 end
 end
 
