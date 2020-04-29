fcd3bce271b44cf3eadc6091d05852de962adc48
mtsch <matijacufar@gmail.com>
2020-04-30 00:59:44 +1200

    make sure all benchmarks take at least a few seconds (#27)

--- benchmark/bench_sparse.jl
+++ benchmark/bench_sparse.jl
@@ -6,11 +6,11 @@ include(joinpath(@__DIR__, "../test/data.jl"))
 Random.seed!(666)
 suite = BenchmarkGroup()
 
-for (n_points, n_tori, dim, modulus) in [(256, 20, 1, 2),
-                                         (512, 10, 1, 3),
-                                         (128, 10, 2, 3),
-                                         (64, 20, 2, 2),
-                                         (64, 10, 3, 2)]
+for (n_points, n_tori, dim, modulus) in [(300, 40, 1, 2),
+                                         (500, 20, 1, 3),
+                                         (200, 20, 2, 3),
+                                         (100, 10, 3, 2),
+                                         (100, 20, 4, 2)]
     suite["$n_tori tori with $n_points points, dim_max=$dim, modulus=$modulus"] =
         @benchmarkable ripserer($(disconnected_tori(n_tori, n_points)),
                                 dim_max=$dim, modulus=$modulus)
