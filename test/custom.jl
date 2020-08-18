8ad77c7781000b8a4ab50041673fa9553acc1a29
mtsch <matijacufar@gmail.com>
2020-08-18 03:58:47 +0200

    improve coverage

--- test/custom.jl
+++ test/custom.jl
@@ -1,7 +1,7 @@
 using Ripserer
 using Test
 
-@testset "Custom filtration" begin
+@testset "Custom filtration 1" begin
     flt = Custom([
         (1,) => 0,
         (4,) => 0,
@@ -23,6 +23,7 @@ using Test
         Simplex{0}(3, 2.0),
         Simplex{0}(4, 0.0),
     ]
+    @test isempty(flt[10])
     @test simplex(flt, Val(2), (1, 2, 4)) === Simplex{2}((4, 2, 1), 8.0)
     @test simplex(flt, Val(2), (4, 3, 1)) === nothing
 
@@ -30,4 +31,24 @@ using Test
     @test d0 == [(0, 3), (0, Inf)]
     @test d1 == [(6, Inf)]
     @test d2 == []
+end
+
+@testset "Custom filtration vs Rips" begin
+    dist = [
+        0 1 2 1
+        1 0 1 2
+        2 1 0 1
+        1 2 1 0
+    ]
+
+    cf = Custom([
+        [(i,) => 0 for i in 1:4];
+        [(i, j) => dist[i, j] for i in 1:4 for j in i+1:4];
+        [(i, j, k) => max(dist[i, j], dist[i, k], dist[j, k])
+         for i in 1:4 for j in i+1:4 for k in j+1:4];
+    ])
+    @test simplex(cf, Val(3), (4, 3, 2, 1)) == nothing
+    @test all(birth(cf, i) == 0 for i in 1:4)
+
+    @test ripserer(dist) == ripserer(cf)
 end
