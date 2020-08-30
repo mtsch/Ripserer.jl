2476b1e6c3af6f32c9d0fbc8c72ad9acf8391ce5
mtsch <matijacufar@gmail.com>
2020-08-30 15:41:46 +1200

    Add information about other Julia packages (#95)

--- docs/make.jl
+++ docs/make.jl
@@ -15,28 +15,32 @@ for example in readdir(EXAMPLES_INPUT, join=true)
     Literate.markdown(example, EXAMPLES_OUTPUT, documenter=true)
 end
 
-makedocs(sitename="Ripserer.jl",
-         format = Documenter.HTML(
-             # Use clean URLs, unless built as a "local" build
-             prettyurls = !("local" in ARGS),
-             assets = ["assets/favicon.ico"],
-         ),
-         pages=[
-             "Home" => "index.md",
-             "Examples" => [
-                 "generated/basics.md",
-                 "generated/stability.md",
-                 "generated/cocycles.md",
-                 "generated/cubical.md",
-                 "generated/malaria.md",
-             ],
-             "Benchmarks" => "benchmarks.md",
-             "API" => [
-                 "Public" => "api/ripserer.md",
-                 "Interfaces" => "api/extensions.md",
-             ],
-             "Acknowledgements and References" => "references.md",
-         ])
+makedocs(
+    sitename="Ripserer.jl",
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = !("local" in ARGS),
+        assets = ["assets/favicon.ico"],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Examples" => [
+            "generated/basics.md",
+            "generated/stability.md",
+            "generated/cocycles.md",
+            "generated/cubical.md",
+            "generated/malaria.md",
+        ],
+        "Benchmarks" => "benchmarks.md",
+        "API" => [
+            "Public" => "api/ripserer.md",
+            "Interfaces" => "api/extensions.md",
+        ],
+        "Related Julia Packages" => "related-work.md",
+        "Acknowledgements and References" => "references.md",
+    ],
+    doctest=false, # Doctests are run as part of testing -- no need to run them twice.
+)
 
 deploydocs(
     repo = "github.com/mtsch/Ripserer.jl.git",
---
+++ docs/src/related-work.md
@@ -0,0 +1,32 @@
+# Related Julia Packages
+
+This section attempts to provide an overview of Julia packages implementing persistent
+homology. If you're trying to do something Ripserer is missing, one of these might have what
+you're looking for.
+
+This list is incomplete and only includes things I'm aware of. The descriptions are based
+on my (limited) experience with these packages.
+
+* [ComputationalHomology.jl](https://github.com/wildart/ComputationalHomology.jl) uses a
+  different, slower algorithm, but can do some things Ripserer can't such as Čech
+  persistent homology and homology of CW complexes. The package is a part of
+  [TDA.jl](https://github.com/wildart/TDA.jl), which also offers other topological data
+  analysis tools like Mapper.
+
+* [Eirene.jl](https://github.com/Eetion/Eirene.jl) uses a [different algorithm based on
+  matroids](https://arxiv.org/abs/1606.00199). A benefit of this algorithm is that it can
+  recover persistent homology generators and a bunch of other data.
+
+* [Ripser.jl](https://github.com/mtsch/Ripser.jl) my deprecated wrapper of the original
+  C++ program. Very bare-bones and outdated. Runs a bit slower than Ripserer.
+
+* [Sparips.jl](https://github.com/mtsch/Ripser.jl) this is a preprocessor that comes with
+  a different wrapper of Ripser. The preprocessor allows you to compute persistent homology
+  of very large datasets. Integrating it with Ripserer is on my TODO list.
+
+* [PersistentCohomology.jl](https://github.com/piever/PersistentCohomology.jl) does
+  essentially the same thing, but with a different algorithm, that is orders of magnitude
+  slower.
+
+If you are a developer of a persistent homology package not on this list, or any of the
+information here is incorrect, let me know or open a PR.
